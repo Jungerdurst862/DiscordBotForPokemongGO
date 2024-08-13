2c39863a6d14f78c1507d03a5a86805855653ddd
Jungerdurst862 <121775185+Jungerdurst862@users.noreply.github.com>
2024-08-13 20:54:11 +0000

    Update start.sh
    s

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ echo "start bash file"
 while true; do
     spawn sshpass -p PaLeKe6280! ssh paul@192.168.1.47
     cd /home/paul/Documents/DiscordBotForPokemongGO
+    python3 main.py
     coordsurl=$(cat 'url.txt')
-    echo "Update Url:"$coordsurl
     sleep 5
 done
 
