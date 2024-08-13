3d52a0a4fc5ead18307581fafc0436b13145c3fc
Jungerdurst862 <paul.lennard.kessel@icloud.com>
2024-08-13 22:02:26 +0000

    w

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "start bash file"
 while true; do
     spawn sshpass -p PaLeKe6280! ssh paul@192.168.1.47
     cd /home/paul/Documents/DiscordBotForPokemongGO
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+shell_exec("start.sh");
+header('Location: http://127.0.0.1:5500?success=true');
+?>
