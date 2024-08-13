dff86194ef47e747ba53e5f9c9028bfc93148943
Jungerdurst862 <121775185+Jungerdurst862@users.noreply.github.com>
2024-08-13 20:49:30 +0000

    Update start.sh
    s

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ while true; do
     spawn sshpass -p PaLeKe6280! ssh paul@192.168.1.47
     cd /home/paul/Documents/DiscordBotForPokemongGO
     coordsurl=$(cat 'url.txt')
+    echo "Update Url:"$coordsurl
     sleep 5
 done
 
