7d30a1e96e6354ae4a26c532fc7f9178910fcc14
Jungerdurst862 <121775185+Jungerdurst862@users.noreply.github.com>
2024-08-13 20:44:02 +0000

    Update start.sh
    s

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ echo "start bash file"
 while true; do
     spawn sshpass -p PaLeKe6280! ssh paul@192.168.1.47
     cd /home/paul/Documents/DiscordBotForPokemongGO
-    python3 main.py
     coordsurl=$(cat 'url.txt')
     sleep 5
 done
